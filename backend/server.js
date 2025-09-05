5562238b571c5d9e29cbaad7cf03fe4e5391f274
aimable <aganzerizen@gmail.com>
2025-09-05 16:54:37 +0200

    4:54 PM 9/5/25

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path'
+import { fileURLToPath } from 'url';
 import connectDB from "./db/db.js";
 import authRoutes from './router/auth.js'
 import contactRoutes from './router/contact.js'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -20,6 +25,11 @@ app.use((err, req, res, next) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
+
+app.use(express.static(path.join(__dirname, '../dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
